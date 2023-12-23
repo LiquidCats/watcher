856bb633b7317b0b70064999048d0ea25e231fab
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-12-23 22:55:02 +0400

    chore: adjust rpc factory

--- internal/app/domain/blockchain/factory.go
+++ internal/app/domain/blockchain/factory.go
@@ -3,6 +3,7 @@ package blockchain
 import (
 	"fmt"
 	"watcher/configs"
+	"watcher/internal/adapter/repository/rpc"
 	"watcher/internal/adapter/repository/rpc/bitcoin"
 	"watcher/internal/adapter/repository/rpc/ethereum"
 	"watcher/internal/app/domain/entity"
@@ -10,11 +11,13 @@ import (
 )
 
 func GetBlockchainRpcRepository(cfg configs.Config) (port.RpcRepository, error) {
+	client := rpc.NewRpcClient(cfg)
+
 	switch cfg.Blockchain {
 	case entity.Ethereum:
-		return ethereum.NewRpcRepository(cfg), nil
+		return ethereum.NewRpcRepository(client), nil
 	case entity.Bitcoin:
-		return bitcoin.NewRpcRepository(cfg), nil
+		return bitcoin.NewRpcRepository(client), nil
 	}
 
 	return nil, fmt.Errorf("unsupported blockchain")

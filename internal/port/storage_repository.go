54019b847322c023e946a14665aac14c247eb76b
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-12-23 21:00:37 +0400

    chore: add method

--- internal/port/storage_repository.go
+++ internal/port/storage_repository.go
@@ -6,8 +6,10 @@ import (
 )
 
 type StorageRepository interface {
-	GetHeight(ctx context.Context, chain string) (entity.BlockHeight, error)
+	GetHeight(ctx context.Context, driver entity.Blockchain) (entity.BlockHeight, error)
 	StoreBlock(ctx context.Context, block *entity.Block) error
+	UpdateBlock(ctx context.Context, newBlock *entity.Block) error
 	GetBlock(ctx context.Context, height entity.BlockHeight) (*entity.Block, error)
 	ConfirmBlock(ctx context.Context, height entity.BlockHeight) error
+	GetAllUnconfirmedBlocks(ctx context.Context, fromHeight entity.BlockHeight) ([]*entity.Block, error)
 }

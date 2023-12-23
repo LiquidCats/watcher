8d20b78646e4f9e5a51b7a7625544fd8843a4d63
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-12-23 22:55:30 +0400

    chore: add additional interface

--- internal/port/rpc_repository.go
+++ internal/port/rpc_repository.go
@@ -5,6 +5,10 @@ import (
 	"watcher/internal/app/domain/entity"
 )
 
+type RpcCaller interface {
+	CallFor(ctx context.Context, dest any, method string, params ...any) error
+}
+
 type RpcRepository interface {
 	GetLatestBlock(ctx context.Context) (*entity.Block, error)
 	GetBlockByHash(ctx context.Context, hash entity.BlockHash) (*entity.Block, error)

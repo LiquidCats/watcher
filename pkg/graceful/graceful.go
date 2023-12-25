ec91e773174d24878e38d3dbcfea832c3369262e
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-12-25 23:15:27 +0400

    chore: adjust graceful run

--- pkg/graceful/graceful.go
+++ pkg/graceful/graceful.go
@@ -2,22 +2,42 @@ package graceful
 
 import (
 	"context"
+	"fmt"
 	"golang.org/x/sync/errgroup"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Runner func(ctx context.Context) error
 
 func Wait(runners ...Runner) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	ctx, cancel := context.WithCancel(context.Background())
 	group, ctx := errgroup.WithContext(ctx)
 
-	for _, runner := range runners {
+	// Goroutine to handle OS signals
+	group.Go(func() error {
+		select {
+		case <-sigs:
+			fmt.Println("Signal received, initiating shutdown")
+			cancel() // cancel the context
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+		return nil
+	})
+
+	// Start a goroutine for each runner
+	for _, r := range runners {
+		runner := r // Capture the current value of r in a local variable
 		group.Go(func() error {
-			return runner(ctx)
+			return runner(ctx) // Use the captured variable here
 		})
 	}
 
+	// Wait for all goroutines to complete
 	return group.Wait()
 }

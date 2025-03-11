708b09e79a4297fba94063e4b98492479f68f42a
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-03-11 22:28:20 +0400

    chore(): update configs

--- configs/app.go
+++ configs/app.go
@@ -1,9 +1,17 @@
 package configs
 
-import "github.com/LiquidCats/watcher/v2/internal/app/kernel/domain/entities"
+import (
+	"time"
+
+	"github.com/LiquidCats/watcher/v2/internal/app/kernel/domain/entities"
+)
 
 type App struct {
-	Driver entities.Driver
-	Type   entities.Type
-	Chain  entities.Chain
+	Driver entities.Driver `yaml:"driver" envconfig:"DRIVER"`
+	Type   entities.Type   `yaml:"type" envconfig:"TYPE"`
+	Chain  entities.Chain  `yaml:"chain" envconfig:"CHAIN"`
+
+	ScanDepth       int           `yaml:"scan_depth" envconfig:"SCAN_DEPTH"`
+	PersistBocks    int           `yaml:"persist_bocks" envconfig:"PERSIST_BOCKS"`
+	PersistDuration time.Duration `yaml:"persist_duration" envconfig:"PERSIST_DURATION"`
 }

1d2d5e12d1a63815ea37affd7f46ded085effadc
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-03-11 22:30:19 +0400

    chore(): remove generic

--- internal/app/utxo/domain/entities/block.go
+++ internal/app/utxo/domain/entities/block.go
@@ -4,7 +4,7 @@ import (
 	"github.com/LiquidCats/watcher/v2/internal/app/kernel/domain/entities"
 )
 
-type Block[T any] struct {
+type Block struct {
 	Height       entities.BlockHeight `json:"height"`
 	Hash         entities.BlockHash   `json:"hash"`
 	PrevHash     entities.BlockHash   `json:"prevHash"`

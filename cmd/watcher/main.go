439603ebc0f59cc8189d714e7c4d5b5805a8b503
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-03-23 20:33:45 +0400

    chore(): run all

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -1,58 +1,84 @@
 package main
 
 import (
-	_ "go.uber.org/automaxprocs"
-
-	"database/sql"
+	"context"
+	"os"
 
 	"github.com/LiquidCats/watcher/v2/configs"
+	"github.com/LiquidCats/watcher/v2/internal/adapter/bus"
+	"github.com/LiquidCats/watcher/v2/internal/adapter/repository/database"
+	"github.com/LiquidCats/watcher/v2/internal/adapter/repository/rpc/utxo"
+	"github.com/LiquidCats/watcher/v2/internal/adapter/state"
+	"github.com/LiquidCats/watcher/v2/internal/app/domain/entities"
+	"github.com/LiquidCats/watcher/v2/internal/app/usecase"
+	"github.com/LiquidCats/watcher/v2/pkg/backbround"
 	"github.com/LiquidCats/watcher/v2/pkg/graceful"
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog"
+
 	_ "github.com/lib/pq"
 	_ "go.uber.org/automaxprocs"
-	"go.uber.org/zap"
 )
 
-const appName = "watcher"
+const app = "watcher"
 
 func main() {
-	log := logger.NewLogger(appName)
-	cfg, err := configs.Load(appName)
+	logger := zerolog.New(os.Stdout).
+		With().
+		Timestamp().
+		Caller().
+		Logger()
+
+	ctx := logger.WithContext(context.Background())
+
+	cfg, err := configs.Load(app)
 	if err != nil {
-		log.Fatal("app: config", zap.Error(err))
+		logger.Fatal().Stack().Err(err).Msg("failed to load config")
 	}
 
-	conn, err := sql.Open(cfg.DB.Driver, cfg.DB.ToDSN())
-	if err != nil {
-		log.Fatal("app: database", zap.Error(err))
-	}
-	defer func(conn *sql.DB) {
-		err := conn.Close()
-		if err != nil {
-			log.Fatal("app: database", zap.Error(err))
+	conn, err := pgx.Connect(ctx, cfg.DB.ToDSN())
+	defer func() {
+		if err := conn.Close(ctx); err != nil {
+			logger.Fatal().Stack().Err(err).Msg("close connection")
 		}
-	}(conn)
-
-	rpc, err := blockchain.GetBlockchainRpcRepository(cfg)
+	}()
 	if err != nil {
-		log.Fatal("app: rpc", zap.Error(err))
+		logger.Fatal().Stack().Err(err).Msg("connect to database")
 	}
 
-	storage := postgres.NewStorageRepository(conn)
-	publisher := redis.NewPublisher(appName, cfg)
-
-	blockConfirmationUsecase := usecase.NewBlockConfirmationUsecase(cfg, rpc, storage, publisher, log.Named("confirmation"))
-	blockHandlingUsecase := usecase.NewBlockHandlingUsecase(cfg, rpc, storage, publisher, log.Named("handling"))
-	cleanupUsecase := usecase.NewCleanupUsecase(cfg, storage, log.Named("cleanup"))
-
-	watcher := async.NewWatcher(log, cfg, blockConfirmationUsecase, blockHandlingUsecase, cleanupUsecase)
-
-	if err := graceful.Wait(
-		watcher.Watch,
-		watcher.Confirm,
-		watcher.Cleanup,
-	); err != nil {
-		log.Fatal("app: graceful", zap.Error(err))
+	if err := database.Migrate(conn); err != nil {
+		logger.Fatal().Stack().Err(err).Msg("migrate")
 	}
 
-	log.Info("app: stopped")
+	repository := database.New(conn)
+	blocksState := state.NewPersisterState[entities.BlockHash](repository)
+	transactionState := state.NewPersisterState[entities.TxID](repository)
+	publisher := bus.NewRedisPublisher(cfg.Redis)
+
+	runners := append(
+		[]graceful.Runner{},
+		graceful.Signals,
+	)
+
+	switch cfg.App.Type {
+	case entities.TypeUtxo:
+		client := utxo.NewClient(cfg.Utxo.Rpc)
+
+		mempoolUseCase := usecase.NewMempoolProcessor(cfg.App, transactionState, client, publisher)
+		blocksUseCase := usecase.NewBlocksProcessor(cfg.App, blocksState, client, publisher, publisher)
+
+		runners = append(runners,
+			backbround.BackgroundRunner("utxo.rpc.blocks", cfg.App.ScanInterval, blocksUseCase),
+			backbround.BackgroundRunner("utxo.rpc.mempool", cfg.App.ScanInterval, mempoolUseCase),
+		)
+	case entities.TypeEvm:
+		logger.Fatal().Msg("not implemented yet")
+	}
+
+	logger.Info().Msg("starting application")
+	if err := graceful.WaitContext(ctx, runners...); err != nil {
+		logger.Fatal().Err(err).Msg("application terminated")
+	}
+
+	logger.Info().Msg("shutting down")
 }

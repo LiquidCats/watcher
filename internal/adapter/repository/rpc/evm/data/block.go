d0c4f92ce231deb34231e1e0de9a196fde5ee44c
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-03-29 18:04:11 +0400

    chore(): add evm client

---
+++ internal/adapter/repository/rpc/evm/data/block.go
@@ -0,0 +1,71 @@
+package data
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/LiquidCats/watcher/v2/internal/app/domain/entities"
+)
+
+type HexUint64 string
+
+func (x HexUint64) Uint64() uint64 {
+	v := string(x)
+	if strings.HasPrefix(v, "0x") {
+		v = strings.TrimPrefix(v, "0x")
+	}
+
+	i, _ := strconv.ParseUint(v, 16, 64)
+
+	return i
+}
+
+func (x HexUint64) String() string {
+	return string(x)
+}
+
+type Block struct {
+	Difficulty      HexUint64          `json:"difficulty"`
+	ExtraData       string             `json:"extraData"`
+	GasLimit        string             `json:"gasLimit"`
+	GasUsed         string             `json:"gasUsed"`
+	Hash            entities.BlockHash `json:"hash"`
+	LogsBloom       string             `json:"logsBloom"`
+	Miner           string             `json:"miner"`
+	MixHash         string             `json:"mixHash"`
+	Nonce           HexUint64          `json:"nonce"`
+	Number          HexUint64          `json:"number"`
+	ParentHash      entities.BlockHash `json:"parentHash"`
+	ReceiptsRoot    string             `json:"receiptsRoot"`
+	Sha3Uncles      string             `json:"sha3Uncles"`
+	Size            HexUint64          `json:"size"`
+	StateRoot       string             `json:"stateRoot"`
+	Timestamp       string             `json:"timestamp"`
+	TotalDifficulty string             `json:"totalDifficulty"`
+	Transactions    []*Transaction     `json:"transactions"`
+}
+
+type LatestBlock struct {
+	Hash entities.BlockHash `json:"hash"`
+}
+
+func (b *Block) GetHeight() entities.BlockHeight {
+	return entities.BlockHeight(b.Number.Uint64())
+}
+
+func (b *Block) GetHash() entities.BlockHash {
+	return b.Hash
+}
+
+func (b *Block) GetPrevHash() entities.BlockHash {
+	return b.ParentHash
+}
+
+func (b *Block) GetTransactions() []entities.Transaction {
+	txs := make([]entities.Transaction, len(b.Transactions))
+	for i, tx := range b.Transactions {
+		txs[i] = tx
+	}
+
+	return txs
+}
---
+++ internal/adapter/repository/rpc/evm/data/transaction.go
@@ -0,0 +1,30 @@
+package data
+
+import "github.com/LiquidCats/watcher/v2/internal/app/domain/entities"
+
+type Transaction struct {
+	BlockHash        entities.BlockHash `json:"blockHash"`
+	BlockNumber      HexUint64          `json:"blockNumber"`
+	From             string             `json:"from"`
+	Gas              string             `json:"gas"`
+	GasPrice         string             `json:"gasPrice"`
+	Hash             entities.TxID      `json:"hash"`
+	Input            string             `json:"input"`
+	Nonce            string             `json:"nonce"`
+	To               string             `json:"to"`
+	TransactionIndex string             `json:"transactionIndex"`
+	Value            string             `json:"value"`
+	Type             string             `json:"type"`
+	ChainId          string             `json:"chainId"`
+	V                string             `json:"v"`
+	R                string             `json:"r"`
+	S                string             `json:"s"`
+}
+
+func (t Transaction) GetTxID() entities.TxID {
+	return t.Hash
+}
+
+func (t Transaction) GetBlockHash() entities.BlockHash {
+	return t.BlockHash
+}

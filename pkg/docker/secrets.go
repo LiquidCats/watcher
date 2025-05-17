89b3c45d21133275493656cafa3bcaadc79fe774
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-05-17 22:24:31 +0400

    feat(): docker secret support

---
+++ pkg/docker/secrets.go
@@ -0,0 +1,32 @@
+package docker
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/go-faster/errors"
+)
+
+const secretPrefix = "/dev/secrets/"
+
+func GetSecret(name string) (string, error) {
+	if strings.HasPrefix(name, secretPrefix) {
+		file, err := os.OpenFile(name, os.O_RDONLY, 0600)
+		if err != nil {
+			return "", errors.Wrap(err, "failed to open secret file")
+		}
+		defer file.Close()
+
+		var buff []byte
+
+		_, err = io.ReadFull(file, buff)
+		if err != nil {
+			return "", errors.Wrap(err, "failed to read secret file")
+		}
+
+		return string(buff), nil
+	}
+
+	return name, nil
+}

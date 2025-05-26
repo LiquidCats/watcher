f7feb9aaef2f9cf0b4667d9ab0ffaa8c6034c900
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-05-26 17:57:30 +0400

    feat: new app config

--- configs/evm.go
+++
@@ -1,9 +0,0 @@
-package configs
-
-type Evm struct {
-	RPC EvmRPC `yaml:"rpc" envconfig:"RPC"`
-}
-
-type EvmRPC struct {
-	URL string `yaml:"url" envconfig:"URL"`
-}
--- configs/load.go
+++ configs/load.go
@@ -1,6 +1,11 @@
 package configs
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v3"
+)
 
 func Load(prefix string) (Config, error) {
 	var cfg Config
@@ -8,6 +13,16 @@ func Load(prefix string) (Config, error) {
 	err := envconfig.Process(prefix, &cfg)
 	if err != nil {
 		return Config{}, err
+	}
+
+	file, err := os.OpenFile(".app.cfg.yaml", os.O_RDONLY, 0677)
+	if err != nil {
+		return cfg, err
+	}
+
+	decoder := yaml.NewDecoder(file)
+	if err = decoder.Decode(&cfg); err != nil {
+		return cfg, err
 	}
 
 	return cfg, nil

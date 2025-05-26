3b9accacb7a79523656a4a640cce84f71a01bbfe
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-05-26 18:10:39 +0400

    chore: update rpc repo

--- internal/adapter/repository/rpc/factory.go
+++ internal/adapter/repository/rpc/factory.go
@@ -1,22 +1,21 @@
 package rpc
 
 import (
-	"fmt"
-
 	"github.com/LiquidCats/watcher/v2/configs"
 	"github.com/LiquidCats/watcher/v2/internal/adapter/repository/rpc/evm"
 	"github.com/LiquidCats/watcher/v2/internal/adapter/repository/rpc/utxo"
 	"github.com/LiquidCats/watcher/v2/internal/app/domain/entities"
 	"github.com/LiquidCats/watcher/v2/internal/app/port/rpc"
+	"github.com/rotisserie/eris"
 )
 
-func Factory(t entities.Type, cfg configs.Config) (rpc.Client, error) {
-	switch t {
+func Factory(cfg configs.ChainConfig) (rpc.Client, error) {
+	switch cfg.Type {
 	case entities.TypeEvm:
-		return evm.NewClient(cfg.Evm.RPC), nil
+		return evm.NewClient(cfg.RPC), nil
 	case entities.TypeUtxo:
-		return utxo.NewClient(cfg.Utxo.RPC), nil
+		return utxo.NewClient(cfg.RPC), nil
 	}
 
-	return nil, fmt.Errorf("factory: unknown type: %v", t)
+	return nil, eris.Errorf("factory: unknown type: %v", cfg.Type)
 }

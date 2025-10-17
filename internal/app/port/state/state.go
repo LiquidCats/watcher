438d7b6319327e78e14a2e8b9c1d9e3186db4093
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-10-17 21:28:47 +0400

    chore: simplify State interface by removing context usage

--- internal/app/port/state/state.go
+++ internal/app/port/state/state.go
@@ -1,10 +1,6 @@
 package state
 
-import (
-	"context"
-)
-
 type State[T any] interface {
-	Set(ctx context.Context, key string, value T) error
-	Get(ctx context.Context, key string) ([]T, error)
+	Set(value T)
+	Get() []T
 }

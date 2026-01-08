58c4e848dc62bab1167f92ee4069699580adbb76
Ilia Shabanov <ishabanov@liquid-cats.com>
2026-01-08 21:10:44 +0100

    chore: Refactor state interfaces and DB contract
    Rename State to SliceState and add MapState Update StateDB to
    SetBlockState(ctx, chain, blocks) using entities types

--- internal/app/port/database/state.go
+++ internal/app/port/database/state.go
@@ -3,10 +3,13 @@ package database
 import (
 	"context"
 
-	"github.com/LiquidCats/watcher/v2/internal/adapter/database"
+	"github.com/LiquidCats/watcher/v2/internal/app/domain/entities"
 )
 
 type StateDB interface {
-	GetStateByKey(ctx context.Context, key string) (database.State, error)
-	SetState(ctx context.Context, arg database.SetStateParams) error
+	SetBlockState(
+		ctx context.Context,
+		chain entities.Chain,
+		blocks []entities.BlockHash,
+	) error
 }
--- internal/app/port/state/state.go
+++ internal/app/port/state/state.go
@@ -1,6 +1,13 @@
 package state
 
-type State[T any] interface {
+type SliceState[T any] interface {
 	Set(value T)
 	Get() []T
+}
+
+type MapState[K comparable, V any] interface {
+	Set(key K, value V)
+	Get(key K) (V, bool)
+	Del(key K)
+	Has(key K) bool
 }
